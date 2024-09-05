9e30e5407eea09b5ed048bac07bd04dd86a2bfee
The Onion Ninja <kay.sfwgithub@cutefuzzy.net>
2024-09-05 16:45:50 -0500

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# sdvspriteimageindexer
-Stardew Sprite Image Indexer
+# Stardew Sprite Image Indexer
+
+Python script to add index numbers to Image spritesheets, starting with 0 in the top left corner.
+Resulting files are doubled in size for legibility.
+
+Made by request for a Mac user who wanted similar functionality to the Windows only script by Calcifer.
+
+![Enlarged and numbered](https://github.com/[scriptsforweirdos]/spriteimageindexer/samplimg.png)
+
+## Requirements
+
+* Python 3
+* Pillow
+
+## Usage
+
+Put any images you wish to add in to the "in" directory.
+
+Inbound files should use the name convention "yourfilename-w-h.ext" where w and h are the width and height of a cell.
+
+For example:
+* If you want to index "springobjects.png", place it in the "in" directory with the name "springobjects-16-16.png".
+* If you want to index an NPC spritesheet such as "Characters/Abigail.png", use the name "Abigail-16-32.png"
+
+**Run the script with `python convert.py`**
+
+Images will be taken from the "in" folder, moved to the "processed" folder afterwards. The enlarged image with numbers will be placed in the "out" folder, with the name "yourfilename-indexed".
---
+++ convert.py
@@ -0,0 +1,60 @@
+import os
+from PIL import ImageDraw, ImageFont, Image
+
+
+def getFiles():
+    inbound = "in"
+    files = [f for f in os.listdir(inbound) if os.path.isfile(os.path.join(inbound, f))]
+    return files
+
+
+def makeGrid(fileName):
+    # define the fonts
+    fontFace = ImageFont.truetype("Hasubi.ttf", 15)
+    fillcolor = "white"
+    shadowcolor = "black"
+
+    # get the file
+    fileExtension = fileName.rsplit(".", 1)[1]
+    fileParts = fileName.split("-")
+    spriteWidth = int(fileParts[-2])
+    spriteHeight = int(fileParts[-1].split(".")[0])
+
+    # Open image and get dims
+    baseImage = Image.open("in/{}".format(fileName))
+    width, height = baseImage.size
+
+    # Double the size so everything is legible
+    newWidth = width * 2
+    newHeight = height * 2
+    cols = int(width / spriteWidth)
+    rows = int(height / spriteHeight)
+    bigImage = baseImage.resize((newWidth, newHeight), Image.Resampling.NEAREST)
+
+    # Add the text
+    i = 0
+    draw = ImageDraw.Draw(bigImage)
+    for row in range(rows):
+        for col in range(cols):
+            thisX = (spriteWidth * 2) * col
+            thisY = (spriteHeight * 2) * row
+            # Outlined text. First we draw the black outline
+            draw.text((thisX - 1, thisY - 1), str(i), font=fontFace, fill=shadowcolor)
+            draw.text((thisX + 1, thisY - 1), str(i), font=fontFace, fill=shadowcolor)
+            draw.text((thisX - 1, thisY + 1), str(i), font=fontFace, fill=shadowcolor)
+            draw.text((thisX + 1, thisY + 1), str(i), font=fontFace, fill=shadowcolor)
+
+            # Draw the white text on top
+            draw.text((thisX, thisY), str(i), font=fontFace, fill=fillcolor)
+            i += 1  # increment the number
+
+    # save new image
+    bigImage.save("out/{}-indexed.{}".format(fileParts[0], fileExtension))
+    # move source image to processed dir
+    os.rename("in/{}".format(fileName), "processed/{}".format(fileName))
+
+
+if __name__ == "__main__":
+    toConvert = getFiles()
+    for inFile in toConvert:
+        makeGrid(inFile)
